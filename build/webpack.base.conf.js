d811e35066f6ff11ee8ba48e0daf0816d7f32de2
Fanlee <yd_lihuan@163.com>
2017-11-28 09:53:31 +0800

    base 配置别名

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -25,6 +25,7 @@ module.exports = {
     alias: {
       '@': resolve('src'),
       'api': resolve('src/api'),
+      'base': resolve('src/base'),
       'common': resolve('src/common'),
       'components': resolve('src/components'),
     }
