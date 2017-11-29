5f7ed0fa7d3c55a726b924aef80314855e3ec79d
Fanlee <yd_lihuan@163.com>
2017-11-29 14:20:00 +0800

    singer组件开发

--- src/base/scroll/scroll.vue
+++ src/base/scroll/scroll.vue
@@ -19,6 +19,10 @@ export default {
     data: {
       type: Array,
       default: null
+    },
+    listenScroll: {
+      type: Boolean,
+      default: false
     }
   },
   mounted() {
@@ -31,10 +35,18 @@ export default {
       if (!this.$refs.wrapper) {
         return
       }
+
       this.scroll = new BScroll(this.$refs.wrapper, {
         probeType: this.probeType,
         click: this.click
       })
+
+      if (this.listenScroll) {
+        let me = this
+        this.scroll.on('scroll', (pos) => {
+          me.$emit('scroll', pos)
+        })
+      }
     },
     enable() {
       this.scroll && this.scroll.enable()
@@ -44,6 +56,12 @@ export default {
     },
     refresh() {
       this.refresh && this.scroll.refresh()
+    },
+    scrollTo() {
+      this.scroll && this.scroll.scrollTo.apply(this.scroll, arguments)
+    },
+    scrollToElement() {
+      this.scroll && this.scroll.scrollToElement.apply(this.scroll, arguments)
     }
   },
   watch: {
