6121751caef4cef8bc440e4a3b148cb9502af2cd
Fanlee <yd_lihuan@163.com>
2018-02-01 16:47:01 +0800

    引入cube-ui webpack配置修改

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('./utils')
 const config = require('../config')
 const vueLoaderConfig = require('./vue-loader.conf')
+const PostCompilePlugin = require('webpack-post-compile-plugin')
 
 function resolve (dir) {
   return path.join(__dirname, '..', dir)
@@ -77,5 +78,8 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new PostCompilePlugin()
+  ]
 }
